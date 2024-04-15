7daf65ff5232bc1e9fbdfdd8db32ea040d0a7d41
Ergi Lama <ergilana98@gmail.com>
2024-04-15 10:18:30 +0200

    ToDoApp

--- to-do-list/app/components/AddTask.tsx
+++ to-do-list/app/components/AddTask.tsx
@@ -4,18 +4,23 @@ import { CiCirclePlus } from "react-icons/ci";
 import TaskModal from "./Modal";
 import { FormEventHandler, useState } from "react";
 import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from 'uuid';
 const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTextValue, setNewTextValue] = useState<string>("");
 
   const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    let uuid = uuidv4();
     e.preventDefault();
     await addTodo({
-      id: 4,
+      id: uuid,
       text: newTextValue,
     });
     setNewTextValue("");
     setModalOpen(false);
+    router.refresh();
   };
   return (
     <div>
@@ -29,8 +34,8 @@ const AddTask = () => {
       </Button>
       <TaskModal modalOpen={modalOpen} setModalOpen={setModalOpen}>
         <form onSubmit={handleSubmitNewTodo}>
-          <h3 className="font-bold text-lg text-center">Add new task</h3>
-          <div className="modal-action justify-center">
+          <h3 className="font-bold text-lg text-center text-blue-200">Add new task</h3>
+          <div className="modal-action justify-center text-blue-200">
             <input
               value={newTextValue}
               onChange={(e) => setNewTextValue(e.target.value)}
--- to-do-list/app/components/Tasks.tsx
+++ to-do-list/app/components/Tasks.tsx
@@ -1,17 +1,82 @@
+"use client";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import { ITask } from "@/types/tasks";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import TaskModal from "./Modal";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDelete(false);
+    router.refresh()
+  };
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
   return (
-      <TableRow>
-        <TableCell>{task.text}</TableCell>
-        <TableCell>{task.text}</TableCell>
-      </TableRow>
+    <TableRow>
+      <TableCell className="w-full">{task.text}</TableCell>
+      <TableCell className="flex gap-5">
+        <FiEdit
+          onClick={() => setOpenModalEdit(true)}
+          cursor="pointer"
+          className="text-blue-800"
+          size={23}
+        />
+        <TaskModal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg text-center text-blue-200">
+              Edit task
+            </h3>
+            <div className="modal-action justify-center text-blue-200">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </TaskModal>
+        <FiTrash2 onClick={() => setOpenModalDelete(true)} cursor="pointer" className="text-red-500" size={23} />
+        <TaskModal
+          modalOpen={openModalDelete}
+          setModalOpen={setOpenModalDelete}
+        >
+          <h3 className="text-lg text-blue-200">Are you sure you want to delete this taks</h3>
+          <div className="modal-action text-blue-200">
+            <button onClick={() => handleDeleteTask(task.id)} className="btn">
+              Yes
+            </button>
+          </div>
+        </TaskModal>
+      </TableCell>
+    </TableRow>
   );
 };
 
